6cd9c63db17b337ca88e9d296071f9f520902451
Alexis.jac <113447336+NatRenter@users.noreply.github.com>
2024-05-14 08:50:49 -0600

    home

--- src/app/common/services/firestore.service.spec.ts
+++ src/app/common/services/firestore.service.spec.ts
@@ -1,16 +1,33 @@
-import { TestBed } from '@angular/core/testing';
+import { Injectable,inject } from '@angular/core';
+import {Firestore, collection, collectionData, setDoc , doc } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+const {v4: uuidv4} = require ("uuid");
 
-import { FirestoreService } from './firestore.service';
+@Injectable({
+  providedIn: 'root'
+})
+export class FirestoreService {
+  firestore: Firestore =inject(Firestore);
 
-describe('FirestoreService', () => {
-  let service: FirestoreService;
+  constructor() { }
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(FirestoreService);
-  });
+  getCollectionChanges<tipo>(path: string){
+    const refcolecction = collection(this.firestore,path);
+    return collectionData(refcolecction) as Observable<tipo[]>;
+  }
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
+  createDocument(data:any, enlace:string){
+    const document = doc(this.firestore, enlace);
+    return setDoc(document,data);
+  }
+
+  createDocumentID(data:any, enlace:string , idDoc:string){
+    const document = doc(this.firestore,'${enlace}/${idDoc}');
+    return setDoc(document,data);
+  }
+
+  createIdDoc(){
+    return uuidv4();
+  }
+
+}
